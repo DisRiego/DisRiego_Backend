4b25b1732aacb232d68f5a4c1ee62d5a246e0c5e
Oscar Andrade <ojachila@gmail.com>
2025-02-26 22:53:56 -0500

    Mostrar usuario

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from app.database import SessionLocal
 from sqlalchemy import text 
@@ -15,5 +15,21 @@ def check_db():
         db = SessionLocal()
         db.execute(text("SELECT 1"))  # Usa text() para la consulta
         return {"message": "Conexión con la base de datos exitosa"}
+    except Exception as e:
+        return {"error": str(e)}
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
+@app.get("/users")
+def check_db(db: Session = Depends(get_db)):
+    try:
+        result = db.execute(text("SELECT * FROM users"))  # Usa text() para la consulta
+        users = result.fetchall()
+        return {"users": [dict(row._mapping) for row in users]}
     except Exception as e:
         return {"error": str(e)}
