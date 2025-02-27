02295a2d84ceb05fc92e874c8e4b36e9764b4329
Oscar Andrade <ojachila@gmail.com>
2025-02-26 21:49:30 -0500

    Revisando conexion db

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from sqlalchemy.orm import Session
+from app.database import SessionLocal
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Infraestructura lista, backend en Render funcionando"}
+
+@app.get("/check-db")
+def check_db():
+    try:
+        db = SessionLocal()
+        db.execute("SELECT 1")  # Ejecutar una consulta simple
+        return {"message": "Conexión con la base de datos exitosa"}
+    except Exception as e:
+        return {"error": str(e)}
